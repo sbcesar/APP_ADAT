ca91ddf69d2a96b6ca47ebe2f0b82e9034bea95e
sbcesar <148891359+sbcesar@users.noreply.github.com>
2025-02-13 20:40:13 +0100

    uwu 2

--- src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
+++ src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -29,6 +30,11 @@ class UsuarioController {
     private lateinit var tokenService: TokenService
     @Autowired
     private lateinit var usuarioService: UsuarioService
+
+    @GetMapping("/")
+    fun getAllUsuarios(): String {
+        return "Hola uwu"
+    }
 
     @PostMapping("/register")
     fun insert(
